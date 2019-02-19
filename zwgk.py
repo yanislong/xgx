617f34c1d7eda6dd6a8216620e4b5f90bbe2a80e
李海龙 <335916781@qq.com>
2019-02-19 15:15:53 +0800

    add file

--- zwgk.py
+++ zwgk.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import re
+import time
+import sys
 
 import bs4
 import requests
@@ -20,7 +22,8 @@ def getLink(i=1):
     html = getHtml(i)
     link = html.find_all(id="info")
     for i in link:
-        print i.a['href']
+        reslink = i.a['href']
+        getRes(reslink)
 
 def getPageNum():
     global html
@@ -33,8 +36,24 @@ def getPageNum():
 #    print pageNum
     return pageNum
 
+def getRes(url):
+    header = {} 
+    header['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36"
+    r = requests.get(url, headers=header)
+    html = bs4.BeautifulSoup(r.content)
+    title = html.find_all('h2')
+    body = html.find_all(class_="zwgk_lr")
+#    print title[0].string
+#    print body[0]
+    con = str(body[0])
+    conp = "<html><head><meta charset='utf-8'></head>" + con + "</html>"
+    tt = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+    name = title[0].string + tt
+    with open('./document/' + name + '.html','w') as f:
+        f.write(conp)
 
 if __name__ == "__main__":
     html = getHtml()
-    getLink()
-#    getPageNum()
+    num = getPageNum()
+    for i in (1,num):
+        getLink(i)
